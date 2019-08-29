a274a1786aa32d323a7063d34ab6fe630d52a5b7
Kelum Sampath <KelumS@99x.lk>
2019-08-29 10:41:19 +0530

    Add project files.

---
+++ CustomMiddlewarePractice/Middleware/CustomeDebugConsoleMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomMiddlewarePractice.Middleware
+{
+    public class CustomeDebugConsoleMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomeDebugConsoleMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            Debug.WriteLine($"++++Request ask for : {httpContext.Request.Path}");
+            //call next middleware delegate in the pipeline
+            await _next.Invoke(httpContext);
+        }
+
+    }
+}
---
+++ CustomMiddlewarePractice/Middleware/MiddlewareExtentions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomMiddlewarePractice.Middleware
+{
+    public static class MiddlewareExtentions
+    {
+        public static IApplicationBuilder UseCustomeMiddleware(this IApplicationBuilder builder)
+        {
+
+            return builder.UseMiddleware<CustomeDebugConsoleMiddleware>();
+        }
+    }
+}
